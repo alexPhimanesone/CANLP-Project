b51f16e52a110b70573491503e14ae1449bbb703
Alex PHIMANESONE <alexpparcoursup@gmail.com>
2023-06-27 12:42:47 +0200

    Added a template for the lunch knowledge code

---
+++ src/rel_lunch.pl
@@ -0,0 +1,60 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%  Deliberative reasoning & Argumentation %%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/*-------------------------------------------------|
+|* Domain knowledge for CAN                       *|
+|*------------------------------------------------*/
+
+/*
+	Dialogue:
+	=========
+
+	Content to reconstruct:
+	=======================
+
+*/                                                                            
+
+
+	/*------------------------------------------------*/
+	/* Domain knowledge starts here                   */
+	/*------------------------------------------------*/
+
+	/* pay attention to the fact that the following
+	   lines are not Prolog clauses, but will be interpreted
+	   by the program. 
+	   The only Prolog predicates are:
+	   - initial_situation
+	   - action
+	   - default
+	   - preference
+	   - incompatible
+	   -  <===	(physical effects)
+	   -  <---	(results of actions)
+	 */ 
+
+	% initial facts
+
+	
+
+
+	% actions
+
+
+	
+	% default predicates: these predicates are true unless proven false
+
+
+    % defaults values may represent the strength, or typicality, of the default
+
+
+	% causal clauses
+
+
+	
+	% physical consequences
+
+
+
+	% preferences (positive predicates only)
+
